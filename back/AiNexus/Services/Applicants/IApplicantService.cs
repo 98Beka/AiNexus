313edb32598a263220f93c9669e89581ec6507c9
smoldobekov <moldobekovsultangazy.work@gmail.com>
2026-03-30 15:36:08 +0600

    Added ApplicantService

---
+++ back/AiNexus/Services/Applicants/IApplicantService.cs
@@ -0,0 +1,13 @@
+﻿using Library.Dtos.Applicants;
+
+namespace AiNexus.Services.Applicants;
+
+public interface IApplicantService
+{
+    Task<ApplicantDto> SubmitAsync(CreateApplicantRequest request);
+    Task<List<ApplicantDto>> GetApplicantsAsync();
+    Task<ApplicantDto> GetApplicantByIdAsync(Guid id);
+    Task<ApplicantDto> GetApplicantByTokenAsync(string token);
+    Task<ApplicantDto> UpdateTestAsync(Guid id, TestResultRequest request);
+
+}
