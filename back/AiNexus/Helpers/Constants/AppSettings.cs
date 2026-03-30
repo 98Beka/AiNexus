1288bc1fa755c2a16978bd9dcef9ce6694cf5a64
smoldobekov <moldobekovsultangazy.work@gmail.com>
2026-03-30 15:36:12 +0600

    Update AppSettings.cs

--- back/AiNexus/Helpers/Constants/AppSettings.cs
+++ back/AiNexus/Helpers/Constants/AppSettings.cs
@@ -1,10 +1,10 @@
-﻿namespace Library.Helpers.Constants
+﻿namespace Library.Helpers.Constants;
+
+public class AppSettings
 {
-    public class AppSettings
-    {
-        public string Secret { get; set; }
-        public int JwtTokenTTL { get; set; }
-        public int TestTokenTTL { get; set; }
-        public int RefreshTokenTTL { get; set; }
-    }
+    public string Secret { get; set; }
+    public int JwtTokenTTL { get; set; }
+    public int TestTokenTTL { get; set; }
+    public int RefreshTokenTTL { get; set; }
+    public string BaseUrl { get; set; }
 }
