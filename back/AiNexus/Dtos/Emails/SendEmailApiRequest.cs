6b099999d2a23cf082c60c76defd8904d7400877
smoldobekov <moldobekovsultangazy.work@gmail.com>
2026-03-31 12:19:05 +0600

    Create SendEmailApiRequest.cs

---
+++ back/AiNexus/Dtos/Emails/SendEmailApiRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AiNexus.Dtos.Emails;
+
+public class SendEmailApiRequest
+{
+    public string ToEmail { get; set; } = string.Empty;
+    public string ToName { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string HtmlBody { get; set; } = string.Empty;
+}
