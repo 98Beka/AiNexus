31294534c303c308e96caac9a65dbb7eae340878
smoldobekov <moldobekovsultangazy.work@gmail.com>
2026-03-31 12:18:53 +0600

    Create EmailApiSettings.cs

---
+++ back/AiNexus/Constants/EmailApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AiNexus.Constants;
+
+public class EmailApiSettings
+{
+    public string BaseUrl { get; set; } = string.Empty;
+    public string SendEndpoint { get; set; } = "/api/emails/email-send";
+}
+
