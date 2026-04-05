76bfc4ab5ca08e39b35a91c54e510b370acb77da
bekzat <tursunkulovbeka@gmile.com>
2026-04-06 00:25:29 +0600

    Update ApplicantService.cs

--- back/AiNexus/Services/Applicants/Impl/ApplicantService.cs
+++ back/AiNexus/Services/Applicants/Impl/ApplicantService.cs
@@ -89,7 +89,7 @@ public class ApplicantService : BaseService, IApplicantService
                 Preview = a.Preview,
                 Score = _context.TestSessions
                     .Where(ts => ts.ApplicantId == a.Id)
-                    .OrderByDescending(ts => ts.FinishedAt)
+                    .OrderByDescending(ts => ts.StartedAt)
                     .Select(ts => ts.Score)
                     .FirstOrDefault()
             });
@@ -120,7 +120,7 @@ public class ApplicantService : BaseService, IApplicantService
         var response = _mapper.Map<ApplicantDto>(applicant);
 
         var testSession = await _context.TestSessions
-                                .OrderByDescending(ts => ts.FinishedAt)
+                                .OrderByDescending(ts => ts.StartedAt)
                                 .FirstOrDefaultAsync(ts => ts.ApplicantId == id);
 
         if (testSession != null)
